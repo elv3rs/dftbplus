bed535be73e27a035eada095ec7a728412403c4e
Merlin Elvers <elvers@uni-bremen.de>
2025-03-27 17:43:29 +0100

    Rerunning benchmarks 1/2

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -6,7 +6,7 @@ time_waveplot() {
     cd "$1" || exit 1
     echo "Timing $1 system:"
     hyperfine "$WP" \
-        --export-json "../$1-first-dev.json" \
+        --export-json "../$1-newCutoffs-release.json" \
         --cleanup "rm -f waveplot_pin.hsd wp-1-1-4-abs2.cube wp-abs2.cube wp-abs2diff.cube wp-1-1-4-real.cube"
 
     cd ..
