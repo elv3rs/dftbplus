955603d822ae63a4d8d37b002d241207629241ba
Merlin Elvers <elvers@uni-bremen.de>
2025-09-02 12:35:47 +0200

    Spot checks

--- test/app/libwavegrid/unit/molorb/simple.F90
+++ test/app/libwavegrid/unit/molorb/simple.F90
@@ -15,12 +15,26 @@ module test_libwavegrid_simple
   use dftbp_common_status, only : TStatus
   use dftbp_math_simplealgebra, only : determinant33
   use dftbp_dftb_boundarycond, only : TBoundaryConds, boundaryCondsEnum, TBoundaryConds_init
-  use fortuno_serial, only : suite => serial_suite_item, test_list, all_close
+  use fortuno_serial, only : suite => serial_suite_item, test_list, all_close, is_close
   $:FORTUNO_SERIAL_IMPORTS()
   implicit none
 
   private
   public :: tests
+
+  type :: spotCheck
+    integer :: idx(4)
+    real(dp) :: expected
+  end type spotCheck
+
+    real(dp), parameter :: eigVecsReal(6,4) = reshape([ &
+         0.93075335285360816_dp,       5.4749851762277632E-003_dp,   6.5189438567696949E-019_dp,  0.0000000000000000_dp,       &
+        -0.11131233240146322_dp,      -0.11131233240146322_dp,      -0.27821574567737223_dp,     -0.73551457100773243_dp,      &
+        -7.8726661848432967E-018_dp,   0.0000000000000000_dp,      -0.33117590117481399_dp,      -0.33117590117481405_dp,      &
+         2.7154435594648282E-018_dp,  -9.9931138111063277E-018_dp,  0.73160204739108692_dp,       1.1102230246251565E-016_dp,  &
+         0.39813631802212995_dp,      -0.39813631802212995_dp,      3.7150709608707145E-033_dp,  -8.1237292785459783E-033_dp,  &
+         9.7504438592516575E-017_dp,   1.0000000000000000_dp,      -6.6858833593547624E-017_dp,   6.6858833593547636E-017_dp], &
+         & [6,4])
 
 contains
 
@@ -111,9 +125,10 @@ contains
     allocate(geometry%species(3))
     geometry%species = [1, 2, 2]
     allocate(geometry%coords(3,3))
-    geometry%coords(:,1) = [0.0_dp, -1.89_dp, 0.0_dp]
-    geometry%coords(:,2) = [0.0_dp, 0.0_dp, 1.48_dp]
-    geometry%coords(:,3) = [0.0_dp, 0.0_dp, -1.48_dp]
+
+    geometry%coords(:,1) = [0.0_dp, -1.8897259885789233_dp, 0.0_dp]
+    geometry%coords(:,2) = [0.0_dp, 0.0_dp, 1.4797763915205659_dp]
+    geometry%coords(:,3) = [0.0_dp, 0.0_dp, -1.4797763915205659_dp]
     geometry%nSpecies = 2
     geometry%speciesNames = ["O", "H"]
     geometry%tHelical = .false.
@@ -124,11 +139,11 @@ contains
     type(TMolecularOrbital), intent(out) :: molorb
     logical, intent(in), optional :: useRadialLut
     
-    real(dp), parameter :: gridOrigin(3) = [-6.0_dp, -7.8897259885789230_dp, -7.4797763915205664_dp]
+    real(dp), parameter :: gridOrigin(3) = [-5.0_dp, -5.0_dp, -5.0_dp]
     real(dp), parameter :: gridVecs(3,3) = reshape([ &
-        0.12_dp, 0.0_dp, 0.0_dp, &
-        0.0_dp, 0.13889725988578924_dp, 0.0_dp, &
-        0.0_dp, 0.0_dp, 0.14959552783041133_dp], [3,3])
+        0.1_dp, 0.0_dp, 0.0_dp, &
+        0.0_dp, 0.1_dp, 0.0_dp, &
+        0.0_dp, 0.0_dp, 0.1_dp], [3,3])
     type(TSpeciesBasis) :: speciesBasis(2)
     type(TGeometry) :: geometry
     type(TBoundaryConds) :: bconds
@@ -146,49 +161,65 @@ contains
 
   $:TEST("LibwavegridH2O_totChrg_real")
     type(TMolecularOrbital) :: molorb
-    logical, parameter :: useRadialLut = .false.
-    logical, parameter :: useGPU = .true.
+    logical, parameter :: useRadialLut = .true.
+    logical, parameter :: useGPU = .false.
     real(dp), allocatable :: valueOnGrid(:,:,:,:)
-    real(dp), parameter :: eigVecsReal(6,4) = reshape([ &
-         0.93075335285360816_dp,        5.4749851762277632E-003_dp,   6.5189438567696949E-019_dp,   0.0000000000000000_dp, &
-        -0.11131233240146322_dp,      -0.11131233240146322_dp,      -0.27821574567737223_dp,      -0.73551457100773243_dp, &
-        -7.8726661848432967E-018_dp,   0.0000000000000000_dp,      -0.33117590117481399_dp,      -0.33117590117481405_dp, &
-         2.7154435594648282E-018_dp,  -9.9931138111063277E-018_dp,  0.73160204739108692_dp,        1.1102230246251565E-016_dp, &
-         0.39813631802212995_dp,      -0.39813631802212995_dp,        3.7150709608707145E-033_dp,  -8.1237292785459783E-033_dp, &
-         9.7504438592516575E-017_dp,   1.0000000000000000_dp,       -6.6858833593547624E-017_dp,   6.6858833593547636E-017_dp], &
-         & [6,4])
     real(dp), parameter :: occupationVec(4) = [2.0_dp, 2.0_dp, 2.0_dp, 2.0_dp]
-    real(dp) :: gridVol, sumTotChrg
+    real(dp) :: gridVol, actual, expected
+    integer :: i, idx(4)
+    type(spotCheck) :: spotChecks(6) = [ &
+        spotCheck([50, 31, 50, 1], 1.009617874235818_dp), & ! O at (0,-1.89,0)
+        spotCheck([1, 1, 1, 1], 0.0_dp), & ! Grid bounds
+        spotCheck([100, 100, 100, 1], 0.0_dp), & ! Grid bounds
+        spotCheck([51, 51, 51, 1], 0.6092945991929660E-01_dp), & ! Grid Center
+        spotCheck([50, 40, 51, 1], 0.5131651070097035_dp), & ! Symmetry: molecule in YZ plane
+        spotCheck([52, 40, 51, 1], 0.5131651070097035_dp) & ! Symmetric: molecule in YZ plane
+    ]
+
 
     call initMolorbH2O(molorb, useRadialLut)
     gridVol = abs(determinant33(molorb%system%gridVecs))
 
-  
+    ! -- Real (H2O) : Total charge calculation --
     allocate(valueOnGrid(100, 100, 100, 1))
     call getTotalChrg(molorb, eigVecsReal, valueOnGrid, occupationVec, useGPU)
-    sumTotChrg = sum(valueOnGrid) * gridVol
-    print *, "Total charge:", sumTotChrg
-    @:ASSERT(abs(sumTotChrg - 8.007785) < 1.0e-3_dp)
+    ! Check sum over grid
+    expected = 8.0040445629655839_dp
+    actual = sum(valueOnGrid) * gridVol
+    print *, "Total charge:", actual
+    @:CHECK(is_close(actual, expected, 1.0e-4_dp))
+    ! Spot check values
+    do i = 1, size(spotChecks)
+      idx = spotChecks(i)%idx
+      expected = spotChecks(i)%expected
+      actual = valueOnGrid(idx(1), idx(2), idx(3), idx(4))
+      @:CHECK(is_close(actual, expected, 1.0e-6_dp))
+    end do
+
+    ! Real (H2O) : Atomic densities calculation
+    ! Real (H2O) : All states calculation
+    ! OLD   
+    ! CPU     8.004 0445629655839
+    ! GPU     8.004 0445629655839
+    ! CPU LUT 8.004 1122133449658
+    ! GPU LUT 8.004 1087880444035
 
 
 
-    @:ASSERT(.true.)
     ! Cases to handle:
-    ! Both cpu, gpu.
-    ! real : default, totChrg, addDensities, lut
-    ! cmplx: default, totChrg, lut
-    ! TODO: Check that orbital matches expectations#
-    ! if WITH_CUDA, duplicate all and set useGPU = .true.
-    ! H-chain, H2O
-    ! using macros: for every <name> in list
-    ! New testcase_<name>
-    ! allocate buffer
-    ! load input args from file
-    ! call molorb
-    ! load expected output from file
-    ! compare all_close(actual, expected)
-    ! compare sum(actual), sum(expected)
-
+    ! Parametrisation: Run on both cpu, gpu, lut ond and off (template 4x).
+    ! Idea: Generate reference data with old code, spot check values & check sum
+    ! Run all on cpu, gpu, lut, and no lut
+    ! That leaves:
+    ! real (H2O): all states, totChrg, addDensities
+    ! cmplx(H-chain): all states, totChrg
+    !
+    !
+    ! Additional tests:
+    ! Init an sto, and spot check values
+    ! Init sto using lut, check if lut access works
+    !
+    ! todo: Allow mixed lut/direct evaluation in gpu kernel by resampling to uniform luts in fortran
   $:END_TEST()
 
 
