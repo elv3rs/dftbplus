f0f438dd637325e03cb7bae97974a8cbdc63bafd
Merlin Elvers <elvers@uni-bremen.de>
2025-09-03 10:00:50 +0200

    Sto init from LUT test

--- test/app/libwavegrid/unit/molorb/testapp.f90
+++ test/app/libwavegrid/unit/molorb/testapp.f90
@@ -1,10 +1,11 @@
 program testapp
   use fortuno_serial, only : execute_serial_cmd_app, test_list
   use test_libwavegrid_simple, only : simple_tests => tests
+  use test_libwavegrid_slater, only : slater_tests => tests
   implicit none
 
   call execute_serial_cmd_app(test_list([&
-      simple_tests()&
+      simple_tests(), slater_tests() &
     ])&
   )
 
