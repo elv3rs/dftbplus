51184e0cc437db0619ae61318f73385f222a5825
Merlin Elvers <elvers@uni-bremen.de>
2025-09-16 15:30:44 +0200

    Spherical Harmonics Unit tests

--- test/app/libwavegrid/unit/molorb/testapp.f90
+++ test/app/libwavegrid/unit/molorb/testapp.f90
@@ -2,10 +2,11 @@ program testapp
   use fortuno_serial, only : execute_serial_cmd_app, test_list
   use test_libwavegrid_simple, only : simple_tests => tests
   use test_libwavegrid_slater, only : slater_tests => tests
+  use test_libwavegrid_spharmonics, only : spharm_tests => tests
   implicit none
 
   call execute_serial_cmd_app(test_list([&
-      simple_tests(), slater_tests() &
+      simple_tests(), slater_tests(), spharm_tests() &
     ])&
   )
 
